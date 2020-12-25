562f4d436143376a805c51f97ba0a67c2fda4d8e
Akinwale Ayomide <akinwaleayomide55@gmail.com>
2020-12-25 01:13:26 -1200

    edited project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React for free
+          Learn React for $500
         </a>
       </header>
     </div>
